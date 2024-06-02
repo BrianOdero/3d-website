1161c74b7d2a394042bc51d4adfe9b598572cc3b
BrianOdero <odero7537@gmail.com>
2024-06-02 09:49:02 +0300

    implemented the navbar and routing of the whole website pages

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+//setting up the root of the appication
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
